be9216296a4bfdac175fadf5f98fbc8f02d148e2
Marut Singh <singh.marut@gmail.com>
2012-10-02 23:17:09 +0530

    progress

--- moneyrabbit/grails-app/conf/UrlMappings.groovy
+++ moneyrabbit/grails-app/conf/UrlMappings.groovy
@@ -1,26 +1,29 @@
 class UrlMappings {
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+    static mappings = {
+        "/$controller/$action?/$id?" {
+            constraints {
+                // apply constraints here
+            }
+        }
 
-        name traelInsurance: "/travel-insurance" {
+        name travelInsurance: "/travel-insurance" {
             controller = 'travelInsurance'
             action = 'index'
         }
 
-		/*"/"(controller: "home")
-        "/home/"(controller: "home", action: ""){
+        name contactUs: "/contact-us" {
+            controller = 'home'
+            action = 'showContactUs'
+        }
 
-        }*/
+        "/"(controller: "home", action: "") {
+        }
 
         "/TravelInsurance/suggestions"(controller: "travelInsurance", action: "index")
 
         "/CreditCard/suggestions"(controller: "creditCard", action: "getSuggestions")
         "/CreditCard/"(controller: "creditCard", action: "index")
-		"500"(view:'/error')
-	}
+        "500"(view: '/error')
+    }
 }
